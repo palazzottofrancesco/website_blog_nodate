23e214dac99d50cab1df455f0cf817241ffe9d84
Francesco Palazzotto <francescopalazzotto@innocode.it>
2025-07-23 16:46:01 +0200

    Upload module

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Website Blog – Nascondi Data',
+    'version': '18.0.1.0.0',
+    'summary': 'Rimuove la data di pubblicazione dai post del blog',
+    'category': 'Website',
+    'author': 'Tuonome / Tua Azienda',
+    'depends': [
+        'website_blog',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
